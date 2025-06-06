93ff33f9d9e57d3f90feee3652c19534fd15f4b4
b00tahead <smay44@gmail.com>
2025-06-06 18:49:38 -0400

    start setting up storybook

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  stories: ["../packages/components/**/*.stories.js"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  framework: {
+    name: "@storybook/web-components-webpack5",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
